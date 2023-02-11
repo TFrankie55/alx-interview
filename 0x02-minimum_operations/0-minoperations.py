bc146e230b94b34c750d53cf4e37a7c415f2997c
TFrankie55 <towoeye50@gmail.com>
2023-02-10 16:07:17 -0800

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
 def minOperations(n):
+    """To find the minimum operation."""
     if n <= 0:
         return 0
     operations = 0
     i = 2
     while i <= n:
-    """This loop finds all the factors
+        """This loop finds all the factors
     of n and increments the number of
     operations for each factor"""
-        while n % i == 0:
-            operations += i
-            n /= i
+    while n % i == 0:
+        operations += i
+        n /= i
         i += 1
-    return operations + n
+        return operations + n
