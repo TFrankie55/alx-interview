a1793c3b60cfeecb26972366dea07f0a34185584
TFrankie55 <towoeye50@gmail.com>
2023-02-10 16:28:08 -0800

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 
 def minOperations(n):
-    """To find the minimum operation."""
+    """
+    In a text file, there is a single character H. Your text editor can execute
+    only two operations in this file: Copy All and Paste. Given a number n,
+    write a method that calculates the fewest number of operations needed to
+    result in exactly n H characters in the file.
+
+    Returns an integer
+    If n is impossible to achieve, returns 0
+    """
     if n <= 0:
         return 0
     operations = 0
     i = 2
     while i <= n:
-        """This loop finds all the factors
-    of n and increments the number of
-    operations for each factor"""
         while n % i == 0:
             operations += i
             n /= i
