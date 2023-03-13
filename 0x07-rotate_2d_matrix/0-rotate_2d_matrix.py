7688fde459d04bd4420d5c745fb7f2dd5f3a3495
TFrankie55 <towoeye50@gmail.com>
2023-03-13 08:08:19 -0700

    matrix is in place

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,11 +2,12 @@
 
 def rotate_2d_matrix(matrix):
     n = len(matrix)
-    """calculates the length of the matrix """
-    for i in range(n // 2):
-        for j in range(i, n - i - 1):
-            temp = matrix[i][j]
-            matrix[i][j] = matrix[n - j - 1][i]
-            matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
-            matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
-            matrix[j][n - i - 1] = temp
+
+    """ Trnspose the matrix in place """
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        """ Reverse each row of the transposed matrix in place """
+        for i in range(n):
+            matrix[i].reverse()
