706679b7fbfee8ab2dc12630f136e146cbba2620
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2023-03-15 20:57:50 +0100

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def rotate_2d_matrix(matrix):
+    """ Rotates a n x n matrix by 90 degrees """
+     import copy
     matcopy = copy.deepcopy(matrix)
     fwd = 0
     current = 0
