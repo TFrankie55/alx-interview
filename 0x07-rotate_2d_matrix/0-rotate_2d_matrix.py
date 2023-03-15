a88ee5efe92fa2236bb04859c69d23a17b50dbe7
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2023-03-15 20:37:19 +0100

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
 def rotate_2d_matrix(matrix):
-    n = len(matrix)
-    """calculates the length of the matrix """
-    for i in range(n // 2):
-        for j in range(i, n - i - 1):
-            temp = matrix[i][j]
-            matrix[i][j] = matrix[n - j - 1][i]
-            matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
-            matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
-            matrix[j][n - i - 1] = temp
+    matcopy = copy.deepcopy(matrix)
+    fwd = 0
+    current = 0
+    revrs = len(matrix) - 1
+    for start in range(len(matrix)):
+        while fwd < len(matrix):
+            matrix[start][fwd] = matcopy[revrs][current]
+            revrs -= 1
+            fwd += 1
+        current += 1
+        fwd = 0
+        revrs = len(matrix) - 1
