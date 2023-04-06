0380f466536e06fbd2f5df534c45602e3595c488
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2023-04-07 00:03:04 +0100

    corrected indentation

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -31,39 +31,24 @@ def isWinner(x, nums):
         If the winner cannot be determined, return None.
     """
     wins = {"Maria": 0, "Ben": 0}
-    """
-    dict to keep track of wins for each player
-    """
 
     for n in nums:
         maria_turn = True
-        """
-        line to keep track of Maria's turn
-        """
 
         while n > 0:
             found_prime = False
-            """
-            if a prime number is found in the set
-            """
             for j in range(2, n+1):
                 if is_prime(j):
                     found_prime = True
                     n -= j
                     break
             if not found_prime:
-                """
-                if no prime number is found, current player loses
-                """
                 if maria_turn:
                     wins["Ben"] += 1
                 else:
                     wins["Maria"] += 1
                 break
             maria_turn = not maria_turn
-            """
-            switch btw Maria and Ben turn
-            """
 
     if wins["Maria"] > wins["Ben"]:
         return "Maria"
