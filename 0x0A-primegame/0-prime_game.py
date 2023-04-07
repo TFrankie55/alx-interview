86063329a0707bfe81e7db10f355e3a95fa61a3d
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2023-04-07 20:24:34 +0100

    Corrected indentation

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -54,23 +54,101 @@ def isWinner(x, nums):
             if not found_prime:
                 """
                 if no prime number is found, current player loses
-                """
-                if maria_turn:
-                    wins["Ben"] += 1
-                else:
-                    wins["Maria"] += 1
-                break
-            maria_turn = not maria_turn
-            """
-            switch btw Maria and Ben turn
-            """
+Prime Number Game Winner
 
-    if wins["Maria"] > wins["Ben"]:
+
+TOSIN OWOEYE
+Help me with this code
+
+Maria and Ben are playing a game. Given a set of consecutive integers starting from 1 up to and including n, they take turns choosing a prime number from the set and removing that number and its multiples from the set. The player that cannot make a move loses the game.
+
+They play x rounds of the game, where n may be different for each round. Assuming Maria always goes first and both players play optimally, determine who the winner of each game is.
+
+Prototype: def isWinner(x, nums)
+where x is the number of rounds and nums is an array of n
+Return: name of the player that won the most rounds
+If the winner cannot be determined, return None
+You can assume n and x will not be larger than 10000
+You cannot import any packages in this task
+Example:
+
+x = 3, nums = [4, 5, 1]
+First round: 4
+
+Maria picks 2 and removes 2, 4, leaving 1, 3
+Ben picks 3 and removes 3, leaving 1
+Ben wins because there are no prime numbers left for Maria to choose
+Second round: 5
+
+Maria picks 2 and removes 2, 4, leaving 1, 3, 5
+Ben picks 3 and removes 3, leaving 1, 5
+Maria picks 5 and removes 5, leaving 1
+Maria wins because there are no prime numbers left for Ben to choose
+Third round: 1
+
+Ben wins because there are no prime numbers for Maria to choose
+Result: Ben has the most wins
+
+Here's a possible implementation in Python:
+
+python
+Copy code
+def is_prime(num):
+    """
+    Helper function to check if a number is prime
+    """
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def isWinner(x, nums):
+    """
+    Determine the winner of the game for each round and return the name of the player that won the most rounds.
+    """
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if n == 1:
+            # If n is 1, Ben wins automatically as there are no prime numbers for Maria to choose
+            ben_wins += 1
+            continue
+
+        # Initialize a set to keep track of remaining numbers
+        remaining = set(range(1, n + 1))
+
+        # Maria starts the game
+        maria_turn = True
+
+        while True:
+            # Find the next prime number
+            prime = None
+            for num in range(2, n + 1):
+                if num in remaining and is_prime(num):
+                    prime = num
+                    break
+
+            if prime is None:
+                # If no prime number is found, the current player cannot make a move and loses
+                if maria_turn:
+                    ben_wins += 1
+                else:
+                    maria_wins += 1
+                break
+
+            # Remove the prime number and its multiples from the set of remaining numbers
+            for multiple in range(prime, n + 1, prime):
+                remaining.discard(multiple)
+
+            # Switch turn to the other player
+            maria_turn = not maria_turn
+
+    if maria_wins > ben_wins:
         return "Maria"
-    elif wins["Maria"] < wins["Ben"]:
+    elif ben_wins > maria_wins:
         return "Ben"
     else:
-        if wins["Maria"] + wins["Ben"] == 0:
-            return None
-        else:
-            return None
+        return None
