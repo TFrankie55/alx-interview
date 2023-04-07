a64a90b08ff8b119ddc52660626367b6cc2e1420
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2023-04-07 20:40:56 +0100

    corrected indentation and added documentation

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -20,25 +20,26 @@ def is_prime(num):
 
 def isWinner(x, nums):
     """
-    Determine the winner of the game for each round and return the name of the player that won the most rounds.
+    To determine the winner of the game for each round and
+    return the name of the player that won the most rounds.
     """
-    maria_wins = 0
-    ben_wins = 0
+    maria_win = 0
+    ben_win = 0
 
     for n in nums:
         if n == 1:
-            # If n is 1, Ben wins automatically as there are no prime numbers for Maria to choose
-            ben_wins += 1
+            # If n is 1, Ben wins as there are no prime numbers for Maria to choose
+            ben_win += 1
             continue
 
         # Initialize a set to keep track of remaining numbers
         remaining = set(range(1, n + 1))
 
-        # Maria starts the game
+        # This time, Maria starts the game
         maria_turn = True
 
         while True:
-            # Find the next prime number
+            # To find the next prime number
             prime = None
             for num in range(2, n + 1):
                 if num in remaining and is_prime(num):
@@ -46,23 +47,29 @@ def isWinner(x, nums):
                     break
 
             if prime is None:
-                # If no prime number is found, the current player cannot make a move and loses
+                """
+                If no prime number is found, the
+                current player cannot make a move and loses
+                """
                 if maria_turn:
-                    ben_wins += 1
+                    ben_win += 1
                 else:
-                    maria_wins += 1
+                    maria_win += 1
                 break
 
-            # Remove the prime number and its multiples from the set of remaining numbers
+            """
+            To remove the prime number and its multiples
+            from the set of remaining numbers
+            """
             for multiple in range(prime, n + 1, prime):
                 remaining.discard(multiple)
 
-            # Switch turn to the other player
+            # Switch to the other player's turn
             maria_turn = not maria_turn
 
-    if maria_wins > ben_wins:
+    if maria_win > ben_win:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif ben_win > maria_win:
         return "Ben"
     else:
         return None
