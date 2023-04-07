dd1a256b73d04fec726a14e4ccad89df269c65ba
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2023-04-07 21:11:54 +0100

    corrected indentation

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -27,7 +27,7 @@ def isWinner(x, nums):
         return None
     Maria = Ben = 0
     for i in range(x):
-        prime = primes(nums[i])
+        prime = prime(nums[i])
         if len(prime) % 2 == 0:
             Ben += 1
         else:
